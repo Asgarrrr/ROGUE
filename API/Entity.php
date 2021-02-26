194520c3b78c16affeb537eb5f9f15fb95f34e3b
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-26 15:17:25 +0100

    Base Entity API

--- API/Entity.php
+++ API/Entity.php
@@ -1,3 +1,17 @@
 <?php
 
+    $content = trim(file_get_contents("php://input"));
+
+    $data = json_decode($content, true);
+
+    if (!isset($data["ID"]))
+        throw new Exception("ID");
+
+    require "../CLASS/Entity.php";
+    require "../CLASS/DB.php";
+
+    $entity = new Entity($data["ID"], $DB);
+
+    $entity->jsonSerialize();
+
 ?>
