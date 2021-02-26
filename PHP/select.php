64d2386a42da575c4209e410f1da4941b7f0c250
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-26 03:41:26 +0100

    Removed the wild style, changing the form, added the JS for actions and API

--- PHP/select.php
+++ PHP/select.php
@@ -1,127 +1,155 @@
-<style>
 
-    body {
-        font-family: "Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace;
-        color: #FFF7ED;
-    }
+<? include "../CLASS/DB.php"; ?>
 
-    .testform{
-        max-width: 300px;
-        border: 1px solid #FFF7ED;
-        padding: 20px;
-        margin: 0 auto;
-    }
+    <?php
 
-    .rollStats {
-        display: flex;
-        justify-content: space-around;
-    }
+        $stdt = $DB->prepare("
 
-    .rollStats > strong {
-        color: #DB59D0;
-    }
+            SELECT * FROM Heros
 
-    #abl_roll {
-        background-color: transparent;
-        color: white;
-        border: 1px solid #fff7ed;
-        width: 100%;
-        padding: 3px;
-        border-radius: 4px;
-        margin-bottom: 17px;
-    }
+                INNER JOIN Floors  	ON Heros.floor		= Floors._fID
+                INNER JOIN Entity 	ON Heros.baseEntity = Entity._eID
+
+            WHERE Heros._IDUser = 1
+
+        ");
+
+        $stdt->execute(array($_SESSION["_userID"]));
+
+    ?>
+
+    <div class="container">
+
+        <h1>Select à hero</h1>
+
+        <div class="wrapper">
+
+            <?php foreach ($stdt as $key => $value) { ?>
+
+                <form class="card" method="POST" id="<?= $value["_ID"] ?>">
+
+                    <div class="flex" style="margin-bottom: 5px;">
+                        <div class="char_port <?= $value["eName"] ."_". $value["gender"] ?>"></div>
+                        <h2 class="heroName"><?= $value["name"] ?></h2>
+                    </div>
+
+                    <strong><?= "An level ".$value["level"] ." ". $value["eName"]." ". $value["class"] ?></strong>
+
+                    <div class="rollStats">
+
+                        <abbr title="Strength — measuring physical power and carrying capacity"> Str:
+                            <span><?= $value["str_score"] ?></span>
+                        </abbr>
+
+                        <abbr title="Dexterity — measuring agility, balance, coordination and reflexes"> Dex:
+                            <span><?= $value["dex_score"] ?></span>
+                        </abbr>
+
+                        <abbr title="Intelligence — measuring deductive reasoning, cognition, knowledge, memory, logic and rationality"> Int:
+                            <span><?= $value["int_score"] ?></span>
+                        </abbr>
 
 
-</style>
+                        <abbr title="Constitution — measuring endurance, stamina and good health"> Con:
+                            <span><?= $value["con_score"] ?></span>
+                        </abbr>
 
-"  .ﺣ.\a  ؆ﭳ ﭳ\aζ  ϊ )\aζ  -"
+                    </div>
+
+                    <p><?= $value["flore"] ?></p>
+
+                    <button type="button" class="removeHero"  value="<?= $value["_ID"] ?>" name="remove">Delete</button>
+                    <button type="submit" value="<?= $value["_ID"] ?>" name="characterChoice">Start</button>
+
+                </form>
+
+            <?php } ?>
+
+            <form method="post" class="card" id="createHeroForm" onsubmit="return false">
+
+                <h2>Create your Hero</h2>
+
+                <p>
+                    <label for="login">Name</label>
+                    <input type="text" id="name" placeholder="The dead's names didn't matter" required>
+                </p>
+
+                <button type="button" id="abl_roll">Roll ability scores</button>
+
+                <div class="rollStats">
+
+                    <abbr title="Strength — measuring physical power and carrying capacity"> Str:
+                        <span id="str_score">6</span>
+                    </abbr>
+
+                    <abbr title="Dexterity — measuring agility, balance, coordination and reflexes"> Dex:
+                        <span id="dex_score">6</span>
+                    </abbr>
+
+                    <abbr title="Intelligence — measuring deductive reasoning, cognition, knowledge, memory, logic and rationality"> Int:
+                        <span id="int_score">6</span>
+                    </abbr>
+
+                    <abbr title="Constitution — measuring endurance, stamina and good health"> Con:
+                        <span id="con_score">6</span>
+                    </abbr>
+
+                </div>
+
+                <p>
+                    <label for="race">Select race:</label>
+                    <select id="race">
+                        <option value="1">Human</option>
+                        <option value="2">Elf</option>
+                        <option value="3">Dwarf</option>
+                        <option value="4">Halfling</option>
+                        <option value="5">Orc</option>
+                    </select>
+                </p>
+
+                <p>
+                    <label for="class">Select class:</label>
+                    <select id="class">
+                        <option value="Figther">Figther</option>
+                        <option value="Mage">Mage</option>
+                        <option value="Rogue">Rogue</option>
+                    </select>
+                </p>
+
+                <p>
+                    <label for="gender">Select gender:</label>
+                    <select id="gender">
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                    </select>
+                </p>
+
+                <p>
+                    <button type="submit" name="submit" id="createHero">Create</button>
+                </p>
+
+            </form>
+
+        </div>
+
+        <script>
+            const idUser = "<?= $_SESSION["_userID"] ?>";
+        </script>
+
+        <script src="SCRIPT/utils.js"></script>
+        <script src="SCRIPT/createHeros.js"></script>
+
+        <?php
 
 
-<form method="post" class="testform">
 
+            if (isset($_POST["characterChoice"])) {
 
-    <h2>Create your Hero</h2>
+                echo $_POST["hero"];
+                //$_SESSION["CharacterID"] = $_POST["characterChoice"];
 
-    <p>
-        <label for="login">Name</label>
-        <input type="text" name="name" required>
-    </p>
+            }
 
-    <button type="button" id="abl_roll">Roll ability scores</button>
+        ?>
 
-    <div class="rollStats">
-
-        <strong>
-
-            <abbr title="Strength — measuring physical power and carrying capacity"> Str: </abbr>
-            <span id="str_score">8</span>
-
-        </strong>
-
-        <strong>
-
-            <abbr title="Dexterity — measuring agility, balance, coordination and reflexes"> Dex: </abbr>
-            <span id="dex_score">6</span>
-
-        </strong>
-        <strong>
-
-            <abbr title="Intelligence — measuring deductive reasoning, cognition, knowledge, memory, logic and rationality"> Int: </abbr>
-            <span id="int_score">4</span>
-
-        </strong>
-        <strong>
-
-            <abbr title="Constitution — measuring endurance, stamina and good health"> Con: </abbr>
-            <span id="con_score">6</span>
-
-        </strong>
     </div>
-
-    <p>
-        <label for="race">Select race:</label>
-        <select name="race">
-            <option value="0">Human</option>
-            <option value="1">Elf</option>
-            <option value="2">Dwarf</option>
-            <option value="3">Halfling</option>
-            <option value="4">Ork</option>
-        </select>
-    </p>
-
-    <p>
-        <label for="class">Select class:</label>
-        <select name="class">
-            <option value="0">Figther</option>
-            <option value="1">Mage</option>
-            <option value="2">Rogue</option>
-        </select>
-    </p>
-
-    <p>
-        <label for="race">Select gender:</label>
-        <select name="race">
-            <option value="0">Male</option>
-            <option value="1">Female</option>
-            <option value="0">Tractopelle</option>
-        </select>
-    </p>
-
-    <p>
-        <button type="submit" name="submit">-|---- start</button>
-    </p>
-
-
-</form>
-
-<?php
-
-    require "CLASS/Entity.php";
-
-    $test = new Entity('1', $DB);
-
-    echo $test->FunctionName();
-
-?>
-
-<script src="SCRIPT/index.js"></script>
