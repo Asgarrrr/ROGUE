d0590946af20759dcc4c7f6f71665f67cd19814b
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-26 15:18:15 +0100

    Hero api

---
+++ API/Heros.php
@@ -0,0 +1,29 @@
+<?php
+
+    $content = trim(file_get_contents("php://input"));
+
+    $data = json_decode($content, true);
+
+    if (!isset($data["id"]))
+        throw new Exception("You must indicate the identifier of the target");
+
+    if (!isset($data["methode"]))
+        throw new Exception('You must indicate the action to perform.');
+
+
+    require "../CLASS/Heros.php";
+    require "../CLASS/DB.php";
+
+
+    $hero = new Heros($data["id"], $DB);
+
+
+    switch ($data["methode"]) {
+        case 'jsonSerialize':
+                $hero->jsonSerialize();
+            break;
+
+        default:
+            # code...
+            break;
+    }
