af947290051254f3aae46407ac06b711e2f3bc0e
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-26 03:37:45 +0100

    Add JsonSerializable. Clarifies variable names to prevent overwriting.

--- CLASS/Entity.php
+++ CLASS/Entity.php
@@ -1,39 +1,38 @@
 <?php
 
-class Entity {
+    class Entity implements JsonSerializable {
 
-    private $_ID;
-    private $_name;
-    private $_baseStr;
-    private $_baseDex;
-    private $_baseInt;
-    private $_baseCon;
+        private $_ID;
+        private $_name;
+        private $_baseStr;
+        private $_baseDex;
+        private $_baseInt;
+        private $_baseCon;
 
-    public function __construct(int $_ID, PDO $DB) {
+        public function __construct(int $_ID, PDO $DB) {
 
-        // —— Prepared statement for the recuperation of the entity
-        $result = $DB->prepare("SELECT * FROM Entity WHERE _ID = ?");
-        $result->execute(array($_ID));
+            // —— Prepared statement for the recuperation of the entity
+            $result = $DB->prepare("SELECT * FROM Entity WHERE _eID = ?");
+            $result->execute(array($_ID));
 
-        $result = $result->fetch();
+            $result = $result->fetch();
 
-        $this->_ID      = $result["_ID"];
-        $this->_name    = $result["name"];
-        $this->_baseStr = $result["baseStr"];
-        $this->_baseDex = $result["baseDex"];
-        $this->_baseInt = $result["baseInt"];
-        $this->_baseCon = $result["baseCon"];
+            $this->_ID      = $result["_eID"];
+            $this->_name    = $result["eName"];
+            $this->_baseStr = $result["eBaseStr"];
+            $this->_baseDex = $result["eBaseDex"];
+            $this->_baseInt = $result["eBaseInt"];
+            $this->_baseCon = $result["eBaseCon"];
+
+        }
+
+        public function jsonSerialize() {
+
+            echo json_encode(get_object_vars($this));
+
+        }
 
     }
-
-    public function FunctionName()
-    {
-        print_r($this);
-    }
-
-}
-
-
 
 ?>
 
