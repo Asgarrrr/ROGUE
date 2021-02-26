0dba5edbf81ab74589341bf54cc49068aa518c4c
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-02-26 03:44:05 +0100

    Improvements

--- CLASS/DB.php
+++ CLASS/DB.php
@@ -1,23 +1,23 @@
-
-
 <?php
 
+    // —— Tecursively search file hack ;3
+    $level = "";
+
+    while (file_exists("..") && !file_exists($level . "config.php"))
+        $level = $level . "../";
+
     // —— Load configuration file
-    $config = include("config.php");
+    $config = include($level . "config.php");
 
     try {
 
         $DB = new PDO("mysql:host=$config[hostname];dbname=$config[database]", "$config[user]", "$config[password]");
 
+        // —— Set the PDO error mode to exception
+        $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     } catch (PDOException $e) {
-
-        if ($config['dev'])
-            echo "Error ! — " . $e->getMessage();
-
-        die();
-
+        die("Error ! — " . $e->getMessage());
     }
 
 ?>
-
-
