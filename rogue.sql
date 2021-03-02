86a2433c8396c0709f3c7b658f34143f2f635a5f
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-02 17:40:09 +0100

    DARK ELF !

--- rogue.sql
+++ rogue.sql
@@ -1,21 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 01 mars 2021 à 15:36
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Hôte : localhost:8889
+-- Généré le : mar. 02 mars 2021 à 16:39
+-- Version du serveur :  5.7.30
+-- Version de PHP : 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `rogue`
@@ -27,29 +20,27 @@ SET time_zone = "+00:00";
 -- Structure de la table `entity`
 --
 
-DROP TABLE IF EXISTS `entity`;
-CREATE TABLE IF NOT EXISTS `entity` (
-  `_eID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `entity` (
+  `_eID` int(11) NOT NULL,
   `eName` varchar(50) NOT NULL,
   `eBaseStr` int(11) NOT NULL DEFAULT '6',
   `eBaseDex` int(11) NOT NULL DEFAULT '6',
   `eBaseInt` int(11) NOT NULL DEFAULT '6',
   `eBaseDef` int(11) NOT NULL DEFAULT '6',
   `eBaseHP` int(11) NOT NULL DEFAULT '6',
-  `eBaseMP` int(11) NOT NULL DEFAULT '6',
-  PRIMARY KEY (`_eID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  `eBaseMP` int(11) NOT NULL DEFAULT '6'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `entity`
 --
 
 INSERT INTO `entity` (`_eID`, `eName`, `eBaseStr`, `eBaseDex`, `eBaseInt`, `eBaseDef`, `eBaseHP`, `eBaseMP`) VALUES
-(1, 'Human', 6, 6, 6, 6, 6, 6),
-(2, 'Elf', 6, 6, 8, 8, 6, 6),
-(3, 'Dwarf', 6, 4, 6, 8, 6, 6),
-(4, 'Halfling', 4, 8, 6, 6, 6, 6),
-(5, 'Orc', 8, 6, 4, 6, 6, 6),
+(1, 'Human', 6, 6, 6, 6, 20, 15),
+(2, 'Elf', 6, 6, 8, 4, 15, 20),
+(3, 'Dwarf', 6, 4, 6, 8, 25, 5),
+(4, 'Dark-Elf', 8, 8, 4, 4, 10, 10),
+(5, 'Orc', 8, 6, 2, 8, 30, 1),
 (6, 'Dragoon', 50, 2, 1, 10, 100, 15);
 
 -- --------------------------------------------------------
@@ -58,11 +49,9 @@ INSERT INTO `entity` (`_eID`, `eName`, `eBaseStr`, `eBaseDex`, `eBaseInt`, `eBas
 -- Structure de la table `floors`
 --
 
-DROP TABLE IF EXISTS `floors`;
-CREATE TABLE IF NOT EXISTS `floors` (
+CREATE TABLE `floors` (
   `_fID` int(11) NOT NULL,
-  `flore` mediumtext NOT NULL,
-  PRIMARY KEY (`_fID`)
+  `flore` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -78,9 +67,8 @@ INSERT INTO `floors` (`_fID`, `flore`) VALUES
 -- Structure de la table `heros`
 --
 
-DROP TABLE IF EXISTS `heros`;
-CREATE TABLE IF NOT EXISTS `heros` (
-  `_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `heros` (
+  `_ID` int(11) NOT NULL,
   `_IDUser` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `gender` varchar(1) NOT NULL,
@@ -88,25 +76,25 @@ CREATE TABLE IF NOT EXISTS `heros` (
   `class` varchar(10) NOT NULL,
   `level` int(11) NOT NULL DEFAULT '1',
   `experience` int(11) NOT NULL DEFAULT '0',
-  `HP` int(11) NOT NULL,
+  `maxHP` int(11) NOT NULL DEFAULT '20',
+  `HP` int(11) NOT NULL DEFAULT '20',
   `str_score` int(11) NOT NULL,
   `dex_score` int(11) NOT NULL,
   `int_score` int(11) NOT NULL,
   `def_score` int(11) NOT NULL,
-  `floor` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`_ID`),
-  KEY `_IDUser` (`_IDUser`),
-  KEY `baseEntity` (`baseEntity`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8;
+  `floor` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `heros`
 --
 
-INSERT INTO `heros` (`_ID`, `_IDUser`, `name`, `gender`, `baseEntity`, `class`, `level`, `experience`, `HP`, `str_score`, `dex_score`, `int_score`, `def_score`, `floor`) VALUES
-(72, 1, 'Test', 'M', 2, 'Rogue', 1, 0, 0, 9, 10, 12, 10, 1),
-(73, 2, 'SayerS', 'F', 2, 'Rogue', 1, 0, 0, 16, 15, 14, 10, 1),
-(74, 2, 'SreyaS', 'M', 5, 'Figther', 1, 0, 0, 13, 11, 8, 16, 1);
+INSERT INTO `heros` (`_ID`, `_IDUser`, `name`, `gender`, `baseEntity`, `class`, `level`, `experience`, `maxHP`, `HP`, `str_score`, `dex_score`, `int_score`, `def_score`, `floor`) VALUES
+(72, 1, 'Test', 'M', 2, 'Rogue', 1, 0, 0, 0, 9, 10, 12, 10, 1),
+(73, 2, 'SayerS', 'F', 2, 'Rogue', 1, 0, 0, 0, 16, 15, 14, 10, 1),
+(74, 2, 'SreyaS', 'M', 5, 'Figther', 1, 0, 0, 0, 13, 11, 8, 16, 1),
+(76, 1, 'GrosTankSaMere', 'F', 3, 'Mage', 1, 0, 20, 20, 14, 11, 7, 17, 1),
+(77, 1, 'OCTOPUTE', 'F', 4, 'Rogue', 1, 0, 20, 20, 15, 18, 13, 9, 1);
 
 -- --------------------------------------------------------
 
@@ -114,10 +102,8 @@ INSERT INTO `heros` (`_ID`, `_IDUser`, `name`, `gender`, `baseEntity`, `class`, 
 -- Structure de la table `monsters`
 --
 
-DROP TABLE IF EXISTS `monsters`;
-CREATE TABLE IF NOT EXISTS `monsters` (
-  `_ID` int(11) NOT NULL,
-  PRIMARY KEY (`_ID`)
+CREATE TABLE `monsters` (
+  `_ID` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -126,13 +112,11 @@ CREATE TABLE IF NOT EXISTS `monsters` (
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `_ID` int(11) NOT NULL,
   `Login` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
@@ -141,6 +125,64 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`_ID`, `Login`, `Password`) VALUES
 (1, 'Asgarrrr', '$2y$10$/j.toeoNeq0iH8FJaBDhBuiTrC/LuE4s8GD2Z45K3/McVwd3DZ7BG'),
 (2, 'Sayerz', '$2y$10$YtbQapthdVPMpj4rkS/TVuqGQa/3.KZnMwSLGOhEOtbXLlTXtz8fm');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `entity`
+--
+ALTER TABLE `entity`
+  ADD PRIMARY KEY (`_eID`);
+
+--
+-- Index pour la table `floors`
+--
+ALTER TABLE `floors`
+  ADD PRIMARY KEY (`_fID`);
+
+--
+-- Index pour la table `heros`
+--
+ALTER TABLE `heros`
+  ADD PRIMARY KEY (`_ID`),
+  ADD KEY `_IDUser` (`_IDUser`),
+  ADD KEY `baseEntity` (`baseEntity`);
+
+--
+-- Index pour la table `monsters`
+--
+ALTER TABLE `monsters`
+  ADD PRIMARY KEY (`_ID`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`_ID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `entity`
+--
+ALTER TABLE `entity`
+  MODIFY `_eID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `heros`
+--
+ALTER TABLE `heros`
+  MODIFY `_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
@@ -152,8 +194,3 @@ INSERT INTO `users` (`_ID`, `Login`, `Password`) VALUES
 ALTER TABLE `heros`
   ADD CONSTRAINT `heros_ibfk_1` FOREIGN KEY (`baseEntity`) REFERENCES `entity` (`_eID`),
   ADD CONSTRAINT `heros_ibfk_2` FOREIGN KEY (`_IDUser`) REFERENCES `users` (`_ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
