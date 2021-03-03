0e3a3904c4a33b23d9c961adaeb3651262aecf85
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-03 17:37:05 +0100

    Game over

--- PHP/dungeon.php
+++ PHP/dungeon.php
@@ -23,6 +23,11 @@
     </head>
     <body class="center">
 
+        <div id="gameover">
+            <img src="../Assets/gameOver.png" alt="" srcset="">
+            <input type="button" value="Retry ?">
+        </div>
+
         <div id="toggle" class="chartoggle">
 
             <div class="char_port" id="char_port"></div>
