b43620f44c92fd1c331b62d9d46ca83ce1b99a6a
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-04 21:47:54 +0100

    Experience + heal + inn + chest +Class + Damages + ok I stop now; good night ;w;

--- SCRIPT/dungeon.js
+++ SCRIPT/dungeon.js
@@ -27,7 +27,7 @@ class Dungeon {
 
         // —— Defined on the user will face a monster, or meet a chest or a merchant
 
-        const monsters = [...this.Hero.fmonsters, "shop", "chest"];
+        const monsters = [...this.Hero.fmonsters, "inn", "chest"];
 
         this.target = monsters[~~(Math.random() * monsters.length)];
 
@@ -49,6 +49,69 @@ class Dungeon {
             this.updateMonsterUI();
 
             this.loadInteractions();
+        } else {
+
+            const message = document.getElementById("message")
+                , open = document.getElementById("retry")
+                , skip = document.getElementById("nextfloor");
+
+                open.style.display = "inline";
+                skip.style.display = "inline";
+
+            if (this.target === "chest") {
+
+                message.innerText = "You have found a chest, gleaming it seems full of treasure..."
+
+                open.value = "Open the chest";
+
+                skip.value = "Leave the chest behind you";
+
+                open.addEventListener("click", () => {
+
+                    if ( ~~(Math.random() * 5) === 1 ) {
+                        message.innerHTML = "You found a potion, lucky you";
+                        this.Hero.potion++;
+                    } else {
+
+                        const golds = ~~(Math.random() * 30) + 20;
+                        message.innerHTML = `You found ${golds} golds, not bad !`;
+                        this.Hero.gold += golds
+
+                    }
+                    open.disabled = true;
+                    this.save();
+                    this.updateHeroUI();
+
+                }, true);
+
+            }
+
+            if (this.target === "inn") {
+
+                message.innerText = "On the horizon, you can see a shabby little inn, you are thinking of resting ..."
+
+                open.value = "Rest ( - 50 gold )";
+                skip.value = "Go away";
+
+                open.addEventListener("click", () => {
+
+                    if (this.Hero.gold < 50)
+                        return this.log("You don't have enough money to stay here");
+
+                    this.Hero.gold -= 50;
+                    this.Hero.HP = this.Hero.maxHP;
+                    this.Hero.MP = this.Hero.maxMP;
+
+                    this.save();
+                    this.updateHeroUI();
+
+                }, true)
+
+            }
+
+            skip.addEventListener("click", () => window.location.reload(), true);
+
+
         }
 
         this.log(`${this.Hero.name} opens the doors to the ${this.Hero.floor} floor`)
@@ -66,7 +129,8 @@ class Dungeon {
         document.getElementById("race"      ).innerHTML = this.Hero.eName;
         document.getElementById("class"     ).innerHTML = this.Hero.class;
         document.getElementById("exp"       ).innerHTML = this.Hero.experience;
-        document.getElementById("potions"   ).innerHTML = 2; // —— The time I create the backend
+        document.getElementById("gold"      ).innerHTML = this.Hero.gold;
+        document.getElementById("potion"    ).innerHTML = this.Hero.potions;
 
         document.getElementById("str_score" ).innerHTML = this.Hero.str_score;
         document.getElementById("dex_score" ).innerHTML = this.Hero.dex_score;
@@ -76,7 +140,9 @@ class Dungeon {
         document.getElementById("class"     ).innerHTML = this.Hero.class;
         document.getElementById("class"     ).innerHTML = this.Hero.class;
 
-        this.generateHPBar(2, 20)
+        document.getElementById("redoOfHealer").disabled = this.Hero.potions > 0 ? false : true;
+
+        document.querySelector(".chartoggle").style.borderColor = this.Hero.skillsPoint > 0 ? "rgb(229, 201, 48)" : "rgba(255, 255, 255, 0.3)";
 
     }
 
@@ -100,20 +166,7 @@ class Dungeon {
 
     }
 
-    async victory() {
-
-        document.getElementById("monster").style.display = "none";
-        document.getElementById("attack").style.display = "none";
-
-        this.Hero.experience += ( ( ~~( Math.random() * 15 ) + 10 ) * this.Hero.floor );
-
-        if (this.Hero.experience - this.Hero.level * 100  > 0) {
-	        this.Hero.experience = this.Hero.experience - this.Hero.level * 100
-	        this.Hero.level++
-	        this.log(`${this.Hero.name} Gain a level, it is now level ${this.Hero.level} ! `)
-            this.upgrade();
-        }
-
+    async save() {
         await fetch("../API/Heros.php", {
             method: "POST",
             body : JSON.stringify({
@@ -122,6 +175,40 @@ class Dungeon {
                 data    : this.Hero
             })
         });
+    }
+
+    async victory() {
+
+        document.getElementById("monster").style.display = "none";
+        document.getElementById("physical").style.display = "none";
+        document.getElementById("magical").style.display = "none";
+
+        this.Hero.experience += ( ( ~~( Math.random() * 15 ) + 10 ) * this.Hero.floor );
+        this.Hero.gold += ( ( ~~( Math.random() * 10 ) + 5 ) );
+
+        if (this.Hero.experience - this.Hero.level * 100  >= 0) {
+	        this.Hero.experience = this.Hero.experience - this.Hero.level * 100
+
+	        if (this.Hero.class === "Rogue")
+                this.Hero.dex_score++;
+
+	        if (this.Hero.class === "Figther")
+                this.Hero.str_score++;
+
+	        if (this.Hero.class === "Mage")
+                this.Hero.int_score+++;
+
+            this.Hero.skillsPoint+=3;
+            this.Hero.maxHP++
+            this.Hero.maxMP++
+            this.Hero.level++
+
+	        this.log(`${this.Hero.name} Gain a level, it is now level ${this.Hero.level} ! `)
+            this.upgrade();
+        }
+
+        await this.save();
+        this.updateHeroUI();
 
         document.getElementById("level").innerText = this.Hero.level
         document.getElementById("exp").innerText = this.Hero.experience
@@ -136,17 +223,20 @@ class Dungeon {
             window.location.reload();
         }, true);
 
-        nextFBtn.addEventListener("click", () => {
+        nextFBtn.addEventListener("click", async () => {
+
+            await fetch("../API/Heros.php", {
+                method: "POST",
+                body : JSON.stringify({
+                    methode : "saveFloor",
+                    id      : this._heroID,
+                    floor   : ++this.Hero.floor
+                })
+            });
+
+            window.location.reload();
 
         }, true);
-
-        await fetch("../API/Heros.php", {
-            method: "POST",
-            body : JSON.stringify({
-                methode : "nextFloor",
-                id      : this._heroID,
-            })
-        });
     }
 
     async dead() {
@@ -160,7 +250,6 @@ class Dungeon {
         })
 
         document.location.replace( "../?DESTROYME=true" );
-
 
     }
 
@@ -178,73 +267,134 @@ class Dungeon {
 
     loadInteractions() {
 
-        const attack = document.getElementById("attack")
+        const physical = document.getElementById("physical")
+            , magical  = document.getElementById("magical")
+            , potion   = document.getElementById("redoOfHealer");
 
-        attack.addEventListener("click", () => {
+        physical.style.display = "inline"
+        magical.style.display = "inline"
+        potion.style.display = "inline"
 
-            let damages = Math.round(this.Hero.str_score * (100/(100 + (this.target.eBaseDef) * 4)));
+        physical.addEventListener("click", () => this.damages("physical"));
+        magical.addEventListener("click", () => this.damages("magical"));
+        potion.addEventListener("click", () => {
 
-            if ((Math.random() * 100) < this.Hero.dex_score) {
+            if (this.Hero.maxHP === this.Hero.HP)
+                return this.log("D E B I L U")
 
-                damages *= 2;
-                this.log(`Critical hit! ${this.Hero.name} inflicts ${damages} damage to ${this.target.eName}`)
+            console.log("test")
 
-            } else {
+            this.Hero.HP = ( this.Hero.HP + 10 ) > this.Hero.maxHP ? this.Hero.maxHP : this.Hero.HP + 10;
+            this.Hero.potions--
 
-                this.log(`${this.Hero.name} inflicts ${damages} damage to ${this.target.eName}`)
+            this.save();
+            this.updateHeroUI();
 
+        });
+
+    }
+
+    damages(type){
+
+        const physical = document.getElementById("physical")
+            , magical  = document.getElementById("magical");
+
+        physical.toggleAttribute("disabled");
+        magical.toggleAttribute("disabled");
+
+        let damages = Math.round(this.Hero[type === "physical" ? "str_score" : "int_score"] * (100/(100 + (this.target.eBaseDef) * 4)));
+
+
+        if (type === "magical") {
+            if (this.Hero.MP < 3)
+                return this.log("You can't use spell without mana !");
+            else {
+
+                damages *= 1.5;
+                this.Hero.MP -= 3;
+                this.updateHeroUI();
             }
+        }
 
-            this.target.HP -= damages;
+        if ((Math.random() * 100) < this.Hero.dex_score) {
 
-            console.clear();
-            console.table(this.Hero);
-            console.table(this.target);
+            damages *= 2;
+            this.log(`Critical hit! ${this.Hero.name} inflicts ${damages} damage to ${this.target.eName} ${ type === "physical" ? "" : "with fire breath"}`)
 
-            if (this.target.HP <= 0) {
-                return this.victory();
-            } else this.updateMonsterUI()
+        } else {
 
-            attack.toggleAttribute("disabled");
-            setTimeout(() => {
-                attack.toggleAttribute("disabled");
+            this.log(`${this.Hero.name} inflicts ${damages} damage to ${this.target.eName} ${ type === "physical" ? "" : "with fire breath"}`)
 
-                damages = Math.round(this.target.eBaseStr * (100/(100+ (this.Hero.eBaseDef) * 4)));
-                this.Hero.HP -= damages;
+        }
 
-                this.log(`${this.target.eName } responds by inflicting ${damages} damages too.`)
+        this.target.HP -= damages;
 
-                if (this.Hero.HP <= 0)
-                    this.dead();
-                else
-                    this.updateHeroUI();
+        if (this.target.HP <= 0)
+            return this.victory();
+        else
+            this.updateMonsterUI()
 
-            }, 200)
+        setTimeout(() => {
+
+            const damages = Math.round(this.target.eBaseStr * (100/(100+ (this.Hero.eBaseDef) * 4)));
+
+            this.Hero.HP -= damages;
+
+            this.log(`${this.target.eName } responds by inflicting ${damages} damages too.`)
+
+            physical.toggleAttribute("disabled");
+            magical.toggleAttribute("disabled");
+
+            if (this.Hero.HP <= 0)
+                this.dead();
+            else
+                return this.updateHeroUI();
+
+        }, 200)
 
 
-        })
 
     }
 
     upgrade() {
 
-        const str_scoreAdd = document.getElementById("str_scoreAdd").style.display = "block"
-            , dex_scoreAdd = document.getElementById("dex_scoreAdd").style.display = "block"
-            , int_scoreAdd = document.getElementById("int_scoreAdd").style.display = "block"
-            , def_scoreAdd = document.getElementById("def_scoreAdd").style.display = "block";
+        const str_scoreAdd = document.getElementById("str_scoreAdd")
+            , dex_scoreAdd = document.getElementById("dex_scoreAdd")
+            , int_scoreAdd = document.getElementById("int_scoreAdd")
+            , def_scoreAdd = document.getElementById("def_scoreAdd");
 
+            str_scoreAdd.style.display = "inline";
+            dex_scoreAdd.style.display = "inline";
+            int_scoreAdd.style.display = "inline";
+            def_scoreAdd.style.display = "inline";
 
-        str_scoreAdd.addEventListener((e) => setSkill(e));
-
-        dex_scoreAdd.addEventListener((e) => setSkill(e));
-
-        int_scoreAdd.addEventListener((e) => setSkill(e));
-
-        def_scoreAdd.addEventListener((e) => setSkill(e));
+        str_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
+        dex_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
+        int_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
+        def_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
 
     }
 
-    setSkill(arg) {
-        console.log(arg)
-    }
+    async setSkill(arg) {
+
+        const str_scoreAdd = document.getElementById("str_scoreAdd")
+            , dex_scoreAdd = document.getElementById("dex_scoreAdd")
+            , int_scoreAdd = document.getElementById("int_scoreAdd")
+            , def_scoreAdd = document.getElementById("def_scoreAdd");
+
+            this.Hero[arg.slice(0, 9)]++
+            this.Hero.skillsPoint--;
+
+            if (this.Hero.skillsPoint === 0) {
+                str_scoreAdd.style.display = "none";
+                dex_scoreAdd.style.display = "none";
+                int_scoreAdd.style.display = "none";
+                def_scoreAdd.style.display = "none";
+            }
+
+            await this.save();
+            this.updateHeroUI();
+
+        }
+
 }
