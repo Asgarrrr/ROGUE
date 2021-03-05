bc5e5c2447d02cf5ae1194fabb719e92deab168f
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-05 15:59:43 +0100

    DB Update

--- rogue.sql
+++ rogue.sql
@@ -1,21 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 04 mars 2021 à 16:50
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Hôte : localhost:8889
+-- Généré le : ven. 05 mars 2021 à 14:58
+-- Version du serveur :  5.7.30
+-- Version de PHP : 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données : `rogue`
@@ -27,18 +20,16 @@ SET time_zone = "+00:00";
 -- Structure de la table `entity`
 --
 
-DROP TABLE IF EXISTS `entity`;
-CREATE TABLE IF NOT EXISTS `entity` (
-  `_eID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `entity` (
+  `_eID` int(11) NOT NULL,
   `eName` varchar(50) NOT NULL,
   `eBaseStr` int(11) NOT NULL DEFAULT '6',
   `eBaseDex` int(11) NOT NULL DEFAULT '6',
   `eBaseInt` int(11) NOT NULL DEFAULT '6',
   `eBaseDef` int(11) NOT NULL DEFAULT '6',
   `eBaseHP` int(11) NOT NULL DEFAULT '6',
-  `eBaseMP` int(11) NOT NULL DEFAULT '6',
-  PRIMARY KEY (`_eID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `eBaseMP` int(11) NOT NULL DEFAULT '6'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `entity`
@@ -62,21 +53,14 @@ INSERT INTO `entity` (`_eID`, `eName`, `eBaseStr`, `eBaseDex`, `eBaseInt`, `eBas
 -- Structure de la table `floors`
 --
 
-DROP TABLE IF EXISTS `floors`;
-CREATE TABLE IF NOT EXISTS `floors` (
+CREATE TABLE `floors` (
   `_fID` int(11) NOT NULL,
   `flore` mediumtext NOT NULL,
   `fmonster1` int(11) NOT NULL,
   `fmonster2` int(11) NOT NULL,
   `fmonster3` int(11) NOT NULL,
   `fmonster4` int(11) NOT NULL,
-  `fmonster5` int(11) NOT NULL,
-  PRIMARY KEY (`_fID`),
-  KEY `fmonster1` (`fmonster1`),
-  KEY `fmonster2` (`fmonster2`),
-  KEY `fmonster3` (`fmonster3`),
-  KEY `fmonster4` (`fmonster4`),
-  KEY `fmonster5` (`fmonster5`)
+  `fmonster5` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -84,7 +68,8 @@ CREATE TABLE IF NOT EXISTS `floors` (
 --
 
 INSERT INTO `floors` (`_fID`, `flore`, `fmonster1`, `fmonster2`, `fmonster3`, `fmonster4`, `fmonster5`) VALUES
-(1, 'Your adventure begins, the first floor is open to you, ignorant, full of bravery, you move forward without fear, while the darkness gradually begins to consume you.', 1, 6, 5, 4, 3);
+(1, 'Your adventure begins, the first floor is open to you, ignorant, full of bravery, you move forward without fear, while the darkness gradually begins to consume you.', 1, 6, 5, 4, 3),
+(2, 'Small pussy player', 10, 7, 5, 9, 8);
 
 -- --------------------------------------------------------
 
@@ -92,9 +77,8 @@ INSERT INTO `floors` (`_fID`, `flore`, `fmonster1`, `fmonster2`, `fmonster3`, `f
 -- Structure de la table `heros`
 --
 
-DROP TABLE IF EXISTS `heros`;
-CREATE TABLE IF NOT EXISTS `heros` (
-  `_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `heros` (
+  `_ID` int(11) NOT NULL,
   `_IDUser` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `gender` varchar(1) NOT NULL,
@@ -102,33 +86,29 @@ CREATE TABLE IF NOT EXISTS `heros` (
   `class` varchar(10) NOT NULL,
   `level` int(11) NOT NULL DEFAULT '1',
   `experience` int(11) NOT NULL DEFAULT '0',
-  `skillsPoint` int(11) NOT NULL,
-  `gold` int(11) NOT NULL,
-  `potions` int(11) NOT NULL,
+  `skillsPoint` int(11) DEFAULT '0',
+  `gold` int(11) NOT NULL DEFAULT '20',
+  `potions` int(11) NOT NULL DEFAULT '1',
   `maxHP` int(11) NOT NULL DEFAULT '20',
   `HP` int(11) NOT NULL DEFAULT '20',
   `maxMP` int(11) NOT NULL DEFAULT '20',
   `MP` int(11) NOT NULL DEFAULT '20',
-  `str_score` int(11) NOT NULL,
-  `dex_score` int(11) NOT NULL,
-  `int_score` int(11) NOT NULL,
-  `def_score` int(11) NOT NULL,
-  `floor` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`_ID`),
-  KEY `_IDUser` (`_IDUser`),
-  KEY `baseEntity` (`baseEntity`)
-) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+  `str_score` int(11) NOT NULL DEFAULT '6',
+  `dex_score` int(11) NOT NULL DEFAULT '6',
+  `int_score` int(11) NOT NULL DEFAULT '6',
+  `def_score` int(11) NOT NULL DEFAULT '6',
+  `floor` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `heros`
 --
 
 INSERT INTO `heros` (`_ID`, `_IDUser`, `name`, `gender`, `baseEntity`, `class`, `level`, `experience`, `skillsPoint`, `gold`, `potions`, `maxHP`, `HP`, `maxMP`, `MP`, `str_score`, `dex_score`, `int_score`, `def_score`, `floor`) VALUES
-(72, 1, 'Test', 'M', 2, 'Rogue', 1, 0, 0, 0, 0, 20, 20, 20, 20, 9, 10, 12, 10, 1),
-(73, 2, 'SayerS', 'F', 2, 'Rogue', 1, 0, 0, 0, 0, 20, 20, 20, 20, 16, 15, 14, 10, 1),
-(74, 2, 'SreyaS', 'M', 5, 'Figther', 1, 0, 0, 0, 0, 20, 15, 20, 20, 13, 11, 8, 16, 1),
-(76, 1, 'GrosTankSaMere', 'F', 3, 'Mage', 1, 0, 0, 0, 0, 20, 20, 20, 20, 14, 11, 7, 17, 1),
-(77, 1, 'OCTOPUTE', 'F', 4, 'Rogue', 1, 0, 0, 0, 0, 20, 20, 20, 20, 15, 18, 13, 9, 1);
+(73, 2, 'SayerS', 'F', 2, 'Rogue', 1, 0, 0, 0, 0, 20, 20, 20, 20, 16, 15, 14, 10, 2),
+(74, 2, 'SreyaS', 'M', 5, 'Figther', 1, 0, 0, 0, 0, 20, 15, 20, 20, 13, 11, 8, 16, 2),
+(80, 1, 'GrosTankSaMere', 'M', 5, 'Rogue', 5, 356, 0, 149, 72, 24, 24, 24, 24, 1100000, 17, 14, 180000000, 2),
+(84, 3, 'Suceur2000', 'M', 4, 'Rogue', 1, 69, 0, 20, 0, 20, 6, 20, 14, 17, 15, 13, 14, 1);
 
 -- --------------------------------------------------------
 
@@ -136,13 +116,11 @@ INSERT INTO `heros` (`_ID`, `_IDUser`, `name`, `gender`, `baseEntity`, `class`, 
 -- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `_ID` int(11) NOT NULL,
   `Login` varchar(255) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  PRIMARY KEY (`_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `Password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
@@ -150,7 +128,65 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`_ID`, `Login`, `Password`) VALUES
 (1, 'Asgarrrr', '$2y$10$/j.toeoNeq0iH8FJaBDhBuiTrC/LuE4s8GD2Z45K3/McVwd3DZ7BG'),
-(2, 'Sayerz', '$2y$10$YtbQapthdVPMpj4rkS/TVuqGQa/3.KZnMwSLGOhEOtbXLlTXtz8fm');
+(2, 'Sayerz', '$2y$10$YtbQapthdVPMpj4rkS/TVuqGQa/3.KZnMwSLGOhEOtbXLlTXtz8fm'),
+(3, 'SUCEUR2000', '$2y$10$U9flY71ELcm2c2MyYQL.8es5X69fZubynDhayfglVkMxmy.0M67UW');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `entity`
+--
+ALTER TABLE `entity`
+  ADD PRIMARY KEY (`_eID`);
+
+--
+-- Index pour la table `floors`
+--
+ALTER TABLE `floors`
+  ADD PRIMARY KEY (`_fID`),
+  ADD KEY `fmonster1` (`fmonster1`),
+  ADD KEY `fmonster2` (`fmonster2`),
+  ADD KEY `fmonster3` (`fmonster3`),
+  ADD KEY `fmonster4` (`fmonster4`),
+  ADD KEY `fmonster5` (`fmonster5`);
+
+--
+-- Index pour la table `heros`
+--
+ALTER TABLE `heros`
+  ADD PRIMARY KEY (`_ID`),
+  ADD KEY `_IDUser` (`_IDUser`),
+  ADD KEY `baseEntity` (`baseEntity`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`_ID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `entity`
+--
+ALTER TABLE `entity`
+  MODIFY `_eID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `heros`
+--
+ALTER TABLE `heros`
+  MODIFY `_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
@@ -172,8 +208,3 @@ ALTER TABLE `floors`
 ALTER TABLE `heros`
   ADD CONSTRAINT `heros_ibfk_1` FOREIGN KEY (`baseEntity`) REFERENCES `entity` (`_eID`),
   ADD CONSTRAINT `heros_ibfk_2` FOREIGN KEY (`_IDUser`) REFERENCES `users` (`_ID`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
