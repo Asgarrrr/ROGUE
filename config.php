358d0809bb609efb635602c6047a106aad8a0ad2
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-08 12:47:57 +0100

    Add

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+    return array(
+        "hostname"  => "localhost",
+        "database"  => "rogue",
+        "user"      => "root",
+        "password"  => "root",
+        "port"      => "8889",
+
+        // —— Dev to display the key echoes, Prod will not display them.
+        "dev"       =>  "Dev"
+    );
+
+?>
