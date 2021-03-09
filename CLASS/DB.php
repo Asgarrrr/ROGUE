b50528d6a96b8a4b3e3f786277c78d5458bfb4ab
Jérémy Caruelle <36574651+Asgarrrr@users.noreply.github.com>
2021-03-09 15:45:40 +0100

    Fix

--- CLASS/DB.php
+++ CLASS/DB.php
@@ -1,20 +1,17 @@
 <?php
 
     // —— Tecursively search file hack ;3
-    $level = "";
+    // $level = "";
 
-    while (file_exists("..") && !file_exists($level . "config.php"))
-        $level = $level . "../";
+    // while (file_exists("..") && !file_exists($level . "config.php"))
+    //     $level = $level . "../";
 
-    // —— Load configuration file
-    $config = include($level . "config.php");
+    // // —— Load configuration file
+    // $config = include($level . "config.php");
 
     try {
 
-        $DB = new PDO("mysql:host=$config[hostname];dbname=$config[database]", "$config[user]", "$config[password]");
-
-        // —— Set the PDO error mode to exception
-        $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $DB = new PDO("mysql:host=mysql-roguesn1.alwaysdata.net;dbname=roguesn1_sb", "roguesn1_sb", "23w9j423w9j4");
 
     } catch (PDOException $e) {
         die("Error ! — " . $e->getMessage());
