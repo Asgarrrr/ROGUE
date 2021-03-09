0214b6627a3ca01a040de1a9995565b14474d18c
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-09 03:12:05 +0100

    Commentary

--- API/Heros.php
+++ API/Heros.php
@@ -1,23 +1,29 @@
 <?php
 
+    // —— Loads all the parameters of my post method.
     $content = trim(file_get_contents("php://input"));
 
+    // —— Transforms the character string into a JSON object
     $data = json_decode($content, true);
 
+    // —— A entity ID and Methode must be specified
     if (!isset($data["id"]))
         throw new Exception("You must indicate the identifier of the target");
 
     if (!isset($data["methode"]))
         throw new Exception('You must indicate the action to perform.');
 
-
+    // —— Inclusion of the database and Heros class
     require "../CLASS/Heros.php";
     require "../CLASS/DB.php";
 
+    if (!isset($DB))
+        throw new Exception("DATABASE ERROR ——");
 
+    // —— Create new Heros with specified ID
     $hero = new Heros($data["id"], $DB);
 
-
+    // —— Switch between the different methods
     switch ($data["methode"]) {
         case 'jsonSerialize':
                 $hero->jsonSerialize();
