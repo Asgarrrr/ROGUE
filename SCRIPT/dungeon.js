15ac5c84a0fd83a2138e0c3ae08a8a5b86b4aafa
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-09 03:13:29 +0100

    Commentary, Equipement, General fix

--- SCRIPT/dungeon.js
+++ SCRIPT/dungeon.js
@@ -18,19 +18,25 @@ class Dungeon {
             })
         }).then( ( res ) => res.json() );
 
-        console.log(this.Hero);
-
+        // —— If the user has competency points, allows him/her to assign
         if (this.Hero.skillsPoint)
             this.upgrade();
 
+        // —— Allows the opening / closing of the navigation bar
+        const toggleStats = document.getElementById("toggle")
+            , menuStats   = document.getElementById("charscreen");
+
+        toggleStats.addEventListener("click", () => menuStats.classList.toggle("open") , true);
+
+        // —— Calling the refresh function of the player interface
         this.updateHeroUI();
 
         // —— Defined on the user will face a monster, or meet a chest or a merchant
-
         const monsters = [...this.Hero.fmonsters, "inn", "chest"];
 
         this.target = monsters[~~(Math.random() * monsters.length)];
 
+        // —— If it a monster...
         if (!isNaN(this.target)) {
 
             // —— Load character data
@@ -43,17 +49,17 @@ class Dungeon {
                 })
             }).then( ( res ) => res.json() );
 
-            console.table(this.target);
-
             this.target.HP = this.target.eBaseHP;
-            this.updateMonsterUI();
 
+            // —— Updates the monster's interface, and starts the fight
+            this.updateMonsterUI();
             this.loadInteractions();
+
         } else {
 
             const message = document.getElementById("message")
-                , open = document.getElementById("retry")
-                , skip = document.getElementById("nextfloor");
+                , open    = document.getElementById("retry")
+                , skip    = document.getElementById("nextfloor");
 
                 open.style.display = "inline";
                 skip.style.display = "inline";
@@ -102,6 +108,7 @@ class Dungeon {
                     this.Hero.HP = this.Hero.maxHP;
                     this.Hero.MP = this.Hero.maxMP;
 
+                    this.log("You slept well, your HP and MPs have regenerated");
                     this.save();
                     this.updateHeroUI();
 
@@ -111,13 +118,13 @@ class Dungeon {
 
             skip.addEventListener("click", () => window.location.reload(), true);
 
-
         }
 
         this.log(`${this.Hero.name} opens the doors to the ${this.Hero.floor} floor`)
 
     }
 
+    // —— Player interface update function
     updateHeroUI() {
 
         document.getElementById("char_port" ).classList.add(`${this.Hero.eName}_${this.Hero.gender}`);
@@ -144,6 +151,50 @@ class Dungeon {
 
         document.querySelector(".chartoggle").style.borderColor = this.Hero.skillsPoint > 0 ? "rgb(229, 201, 48)" : "rgba(255, 255, 255, 0.3)";
 
+        this.stuff()
+
+        const weaponSelect      = document.getElementById("weapon")
+            , armorSelect       = document.getElementById("armor")
+            , accessorySelect   = document.getElementById("accessory");
+
+        const add = (section, element) => {
+
+
+            let option = document.createElement("option");
+            option.value = element._sID;
+            option.innerHTML = element.sName;
+
+            section.appendChild(option);
+
+            if (this.Hero[section.id] === element._sID)
+                section.value = element._sID;
+
+        }
+
+        weaponSelect.innerHTML = '';
+        this.Hero.stuff[0].forEach( ( weapon ) => add( weaponSelect , weapon) );
+
+        armorSelect.innerHTML = '';
+        this.Hero.stuff[1].forEach( ( armor ) => add( armorSelect , armor) );
+
+        accessorySelect.innerHTML = '';
+        this.Hero.stuff[2].forEach( ( accessory ) => add( accessorySelect , accessory) );
+
+        weaponSelect.addEventListener("change", async (e) => {
+            this.Hero.weapon = parseInt(e.target.value);
+            this.stuff()
+        })
+
+        armorSelect.addEventListener("change", async (e) => {
+            this.Hero.armor = parseInt(e.target.value);
+            this.stuff()
+        })
+
+        accessorySelect.addEventListener("change", async (e) => {
+            this.Hero.accessory = parseInt(e.target.value);
+            this.stuff()
+        })
+
     }
 
     updateMonsterUI() {
@@ -154,6 +205,11 @@ class Dungeon {
     }
 
 
+    /** Generates a HP bar in Ascii art
+     *
+     * @param {Number} maxHP
+     * @param {Number} HP
+     */
     generateHPBar (maxHP, HP) {
 
         let base = new Array(30).fill("░");
@@ -177,6 +233,7 @@ class Dungeon {
         });
     }
 
+    // —— Replaces combat elements, assigns rewards
     async victory() {
 
         document.getElementById("monster").style.display = "none";
@@ -189,14 +246,15 @@ class Dungeon {
         if (this.Hero.experience - this.Hero.level * 100  >= 0) {
 	        this.Hero.experience = this.Hero.experience - this.Hero.level * 100
 
-	        if (this.Hero.class === "Rogue")
-                this.Hero.dex_score++;
+            switch (this.Hero.class) {
+                case "Rogue"    : this.Hero.dex_score++
+                    break;
+                case "Figther"  : this.Hero.str_score++
+                    break;
+                case "Mage"     : this.Hero.int_score++
+                    break;
+            }
 
-	        if (this.Hero.class === "Figther")
-                this.Hero.str_score++;
-
-	        if (this.Hero.class === "Mage")
-                this.Hero.int_score++;
 
             this.Hero.skillsPoint+=3;
             this.Hero.maxHP++
@@ -239,6 +297,7 @@ class Dungeon {
         }, true);
     }
 
+    // —— Request for deletion of the peros
     async dead() {
 
         await fetch("../API/Heros.php", {
@@ -253,6 +312,7 @@ class Dungeon {
 
     }
 
+    // —— Creates a new dom element in the list below
     log(content) {
 
         var oUl = document.getElementById("messages");
@@ -265,6 +325,7 @@ class Dungeon {
 
     }
 
+    // —— Charges the elements of the fight
     loadInteractions() {
 
         const physical = document.getElementById("physical")
@@ -294,6 +355,7 @@ class Dungeon {
 
     }
 
+    // —— Formula for the calculation of damages
     damages(type){
 
         const physical = document.getElementById("physical")
@@ -302,8 +364,11 @@ class Dungeon {
         physical.toggleAttribute("disabled");
         magical.toggleAttribute("disabled");
 
-        let damages = Math.round(this.Hero[type === "physical" ? "str_score" : "int_score"] * (100/(100 + (this.target.eBaseDef) * 4)));
-
+        let damages = ~~(
+            this.Hero[type === "physical" ? "str_score" : "int_score"]
+            + ( this.Hero.stuffBoost[type === "physical" ? "str" : "int"] || 0 )
+            * ( 100 / (100 - (this.target.eBaseDef) * 4))
+        );
 
         if (type === "magical") {
             if (this.Hero.MP < 3)
@@ -316,7 +381,7 @@ class Dungeon {
             }
         }
 
-        if ((Math.random() * 100) < this.Hero.dex_score) {
+        if ((Math.random() * 100) < (this.Hero.dex_score + this.Hero.stuffBoost["dex"] || 0)) {
 
             damages *= 2;
             this.log(`Critical hit! ${this.Hero.name} inflicts ${damages} damage to ${this.target.eName} ${ type === "physical" ? "" : "with fire breath"}`)
@@ -336,7 +401,9 @@ class Dungeon {
 
         setTimeout(() => {
 
-            const damages = Math.round(this.target.eBaseStr * (100/(100+ (this.Hero.eBaseDef) * 4)));
+            const damages = ~~(this.target.eBaseStr * (100/(100+ (this.Hero.eBaseDef + this.Hero.stuffBoost["def"]) * 4)));
+
+            console.log(damages);
 
             this.Hero.HP -= damages;
 
@@ -356,6 +423,7 @@ class Dungeon {
 
     }
 
+    // —— Display of the elements relating to the attribution of competency points
     upgrade() {
 
         const str_scoreAdd = document.getElementById("str_scoreAdd")
@@ -368,10 +436,10 @@ class Dungeon {
             int_scoreAdd.style.display = "inline";
             def_scoreAdd.style.display = "inline";
 
-        str_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
-        dex_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
-        int_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
-        def_scoreAdd.addEventListener("click", (e) => this.setSkill(e.originalTarget.id));
+        str_scoreAdd.addEventListener("click", (e) => this.setSkill(e.target.id));
+        dex_scoreAdd.addEventListener("click", (e) => this.setSkill(e.target.id));
+        int_scoreAdd.addEventListener("click", (e) => this.setSkill(e.target.id));
+        def_scoreAdd.addEventListener("click", (e) => this.setSkill(e.target.id));
 
     }
 
@@ -382,19 +450,53 @@ class Dungeon {
             , int_scoreAdd = document.getElementById("int_scoreAdd")
             , def_scoreAdd = document.getElementById("def_scoreAdd");
 
-            this.Hero[arg.slice(0, 9)]++
-            this.Hero.skillsPoint--;
+        this.Hero[arg.slice(0, 9)]++
+        this.Hero.skillsPoint--;
 
-            if (this.Hero.skillsPoint === 0) {
-                str_scoreAdd.style.display = "none";
-                dex_scoreAdd.style.display = "none";
-                int_scoreAdd.style.display = "none";
-                def_scoreAdd.style.display = "none";
-            }
-
-            await this.save();
-            this.updateHeroUI();
-
+        if (this.Hero.skillsPoint === 0) {
+            str_scoreAdd.style.display = "none";
+            dex_scoreAdd.style.display = "none";
+            int_scoreAdd.style.display = "none";
+            def_scoreAdd.style.display = "none";
         }
 
+        await this.save();
+        this.updateHeroUI();
+
+    }
+
+    // —— Calculation of equipment increase statistics
+    async stuff (){
+
+        this.Hero.stuffBoost = {
+            str: 0,
+            dex: 0,
+            int: 0,
+            def: 0,
+        };
+
+        (this.Hero.stuff[0].concat(this.Hero.stuff[1], this.Hero.stuff[2])).map( x => {
+
+            if ([this.Hero.weapon, this.Hero.armor, this.Hero.accessory].includes(x._sID)) {
+
+                this.Hero.stuffBoost["str"] += x.sStrModifier;
+                this.Hero.stuffBoost["dex"] += x.sDexModifier;
+                this.Hero.stuffBoost["int"] += x.sIntModifier;
+                this.Hero.stuffBoost["def"] += x.sDefModifier;
+
+            }
+
+        });
+
+        document.getElementById("aStr_score" ).innerHTML = this.Hero.stuffBoost["str"];
+        document.getElementById("aDex_score" ).innerHTML = this.Hero.stuffBoost["dex"];
+        document.getElementById("aInt_score" ).innerHTML = this.Hero.stuffBoost["int"];
+        document.getElementById("aDef_score" ).innerHTML = this.Hero.stuffBoost["def"];
+
+        this.save();
+
+    }
+
 }
+
+// —— 500 ;3
