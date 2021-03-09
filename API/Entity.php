ef51de1582b09d6c775beba964c134ac498a4c57
Jérémy Caruelle <36574651+Asgarrrrr@users.noreply.github.com>
2021-03-09 03:12:11 +0100

    Commentary

--- API/Entity.php
+++ API/Entity.php
@@ -1,17 +1,26 @@
 <?php
 
-    $content = trim(file_get_contents("php://input"));
+    // —— Loads all the parameters of my post method.
+    $content = trim( file_get_contents("php://input") );
 
+    // —— Transforms the character string into a JSON object
     $data = json_decode($content, true);
 
+    // —— A entity ID must be specified
     if (!isset($data["ID"]))
-        throw new Exception("ID");
+        throw new Exception("ID Required !");
 
+    // —— Inclusion of the database and Entity class
     require "../CLASS/Entity.php";
     require "../CLASS/DB.php";
 
+    if (!isset($DB))
+        throw new Exception("DATABASE ERROR ——");
+
+    // Create new Entity with specified ID
     $entity = new Entity($data["ID"], $DB);
 
+    // —— Return entity data
     $entity->jsonSerialize();
 
 ?>
--- API/Monsters.php
+++ API/Monsters.php
@@ -1,30 +1,36 @@
 <?php
 
+    // —— Loads all the parameters of my post method.
     $content = trim(file_get_contents("php://input"));
 
+    // —— Transforms the character string into a JSON object
     $data = json_decode($content, true);
 
+    // —— A monster ID and Methode must be specified
     if (!isset($data["id"]))
         throw new Exception("You must indicate the identifier of the target");
 
     if (!isset($data["methode"]))
         throw new Exception('You must indicate the action to perform.');
 
-
+    // —— Inclusion of the database and Monsters class
     require "../CLASS/Monsters.php";
     require "../CLASS/DB.php";
 
+    if (!isset($DB))
+        throw new Exception("DATABASE ERROR ——");
 
+    // —— Create new Monsters with specified ID
     $monster = new Monsters($data["id"], $DB);
 
-
+    // —— Switch between the different methods
     switch ($data["methode"]) {
         case 'jsonSerialize':
                 $monster->Difficulty($data["floor"]);
                 $monster->jsonSerialize();
             break;
+
         default:
-            # code...
             break;
     }
 
